b2659cda8e08baf9ee6c4b453feb4b4c530c7ade
abdunnahid <abdun.nahid@gmail.com>
2022-04-14 11:46:07 +0600

    ✅ Solve LeetCode: 700. Search in a Binary Search Tree

---
+++ leetcode/search-in-a-binary-search-tree/search-in-a-binary-search-tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function (root, val) {
+
+    let node = root;
+
+    while (node) {
+        if (val === node.val) {
+            return node;
+        }
+        else if (val > node.val) {
+            node = node.right;
+        }
+        else {
+            node = node.left;
+        }
+    }
+    return null;
+};
