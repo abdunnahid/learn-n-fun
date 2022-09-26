e7231a2a1de12f61d117ee4f31abdb78b0e30d0e
abdunnahid <abdun.nahid@gmail.com>
2022-09-26 07:47:27 +0600

    ⁉️ Solve LeetCode: 17. Letter Combinations of a Phone Number
    Study Backtracking

---
+++ leetcode/letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function (digits) {
+
+    const letterMap = {
+        2: 'abc',
+        3: 'def',
+        4: 'ghi',
+        5: 'jkl',
+        6: 'mno',
+        7: 'pqrs',
+        8: 'tuv',
+        9: 'wxyz'
+    }
+
+
+};
+
+const result = letterCombinations("23");
+console.log("Result: ", result);
